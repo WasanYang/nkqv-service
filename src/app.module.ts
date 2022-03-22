0a92891a67f522565975dbe832d45ed1f18d8dc7
Wasan Yang <wy@manaosoftware.com>
2022-03-22 23:58:07 +0700

    setup multi env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { configuration } from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env.dev' })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/${process.env.NODE_ENV}.env`,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
